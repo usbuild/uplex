90ae710bc248bfb52e6bfe24a509635d60f2f703
Usbuild <njuzhangqichao@gmail.com>
2012-10-09 20:32:00 +0800

    add basic output file skeleton and define the input file structure

---
+++ output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+import re
+regexps = [r'[\n]',
+r'[0-9]+',
+r'[0-9]*\.[0-9]+',
+r'[a-zA-Z][a-zA-Z0-9]*',
+r'[\+\-\*\/\%]',
+r'.',]
--- uplex.py
+++ uplex.py
@@ -5,31 +5,37 @@ import  re
 
 f = open('sample.l', 'r')
 ST_DEFAULT = 0
-ST_MACRO = 1
-ST_RAW = 2
-ST_MAIN = 3
-state = ST_MACRO
-
-def handle_macro(line):
-    print line
-
-
-def handle_raw(line):
-    print '''\
-re = hello\
-    '''
-    pass
-
-
-def handle_main(line):
-    print line
-
+ST_RAW = 1
+ST_MAIN = 2
+ST_DEFINE = 3
+state = ST_DEFAULT
+with open('skeleton.skl', 'r') as skl:
+    skl_str = skl.read()
+output = ''
+regexps = {}
 main_status = (ST_MAIN, ST_RAW)
 cur_regexp = ''
 
+def handle_default(line):
+    global output
+    output += line
+    pass
+
+def handle_raw(line):
+    global cur_regexp
+    global regexps
+    regexps[cur_regexp] += line
+    pass
+
+def handle_main(line):
+    pass
+
+def handle_define(line):
+    pass
+
 handle_map = {
-    ST_DEFAULT: lambda x: None,
-    ST_MACRO: lambda x: handle_macro(x),
+    ST_DEFAULT: lambda x: handle_default(x),
+    ST_DEFINE: lambda x: handle_define(x),
     ST_RAW: lambda x: handle_raw(x),
     ST_MAIN: lambda x: handle_main(x),
 }
@@ -39,18 +45,27 @@ for line in f:
         continue
 
     line = line.rstrip()
-    if line == '%%' and state not in main_status:
+    if line == '%#' and state != ST_DEFINE:
+        state = ST_DEFINE
+        continue
+    elif line == '%#' and state == ST_DEFINE:
+        state = ST_DEFAULT
+        continue
+    elif line == '%%' and state not in main_status:
         state = ST_MAIN
         continue
     elif line == '%%' and state in main_status:
-        state = ST_MACRO
+        state = ST_DEFAULT
         continue
     elif len(line) > 2 and line[0:2] == '->' and state in main_status:
         cur_regexp = line[2:]
+        regexps[cur_regexp] = ''
         state = ST_RAW
         continue
-
     handle_map[state](line)
+
+result = skl_str.replace('%{content}', output)
+print regexps
 
 f.close()
 
