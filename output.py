f969ea511ee75efca2ee99a87ddc69409d0fca82
Usbuild <njuzhangqichao@gmail.com>
2012-10-10 10:54:53 +0800

    now can run a simple with built in re

--- output.py
+++ output.py
@@ -3,73 +3,35 @@
 
 import re
 
+def search(raw, reg_c):
+    rreg,  rdata = None, None
+
+    for reg,reg_c in reg_c:
+        match = reg_c.search(raw)
+        if match != None:
+            t1, t2 = reg_c, match.span()[1]
+            if rdata == None or t2 > rdata:
+                rreg,  rdata = reg,  t2
+    return (rreg, rdata)
+
 regexps = [r'[\n]',
            r'[0-9]+',
            r'[0-9]*\.[0-9]+',
            r'[a-zA-Z][a-zA-Z0-9]*',
            r'[\+\-\*\/\%]',
            r'.', ]
-strs = ['newline', 'int', 'float', 'alpha', 'operator', 'unknown']
-regexps_c = [re.compile('^' + c) for c in regexps]
 
-def find_match(raw, reg_c=regexps_c):
-    result = []
-    global regexps
-    for reg in reg_c:
-        if reg.match(raw):
-            result.append(reg)
-    return result
-
-
-def search(raw, reg_c=regexps_c):
-    rreg = None
-    rdata = None
-    for reg in reg_c:
-        match = reg.search(raw)
-        if match != None:
-            t1, t2  = reg, match.span()[1]
-            if rdata == None or t2 > rdata:
-                rreg, rdata = reg, t2
-    return (rreg,rdata)
-
+regexps_c = [(r, re.compile('^' + r)) for r in regexps]
 
 input = 'hello %2%31.211'
-tmp = input
-tokens = []
-symbol = []
+#for a in regexps.keys():
+#    print 'elif reg == \'',a,'\':'
+#    print '\t',regexps[a]
+
 while True:
-    if len(tmp) == 0:
-        break
-    reg, data = search(tmp)
+    if len(input) == 0:break
+    reg, data = search(input, regexps_c)
     if reg == None:
-        exit(0)
+        raise SyntaxError
     else:
-        tokens.append(tmp[:data])
-        symbol.append(strs[regexps_c.index(reg)])
-        tmp = tmp[data:]
-
-print tokens
-print symbol
-
-
-
-
-#tokens = []
-#token = ''
-#search_list = regexps_c
-#while len(input_list) > 0:
-#    a = input_list.pop(0)
-#    if len(token) == 0:
-#        tokens.append(token)
-#        search_list = regexps_c
-#    token += a
-#    search_list = find_match(token, search_list)
-#    print a
-#    if len(search_list) == 0:
-#        if len(token) == 1:
-#            raise SyntaxError
-#        token = ''
-#        input_list.insert(0, a)
-#        print tokens[-1]
-#    else:
-#        tokens[-1] = token
+        input = input[data:]
